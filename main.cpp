87bc40389b99eac931efb6b16b10b1abc64f708f
unknown <maksimowiczmateusz13@gmail.com>
2022-11-01 22:37:37 +0100

    font

--- main.cpp
+++ main.cpp
@@ -1,24 +1,35 @@
-﻿#include "Menu.hpp"
+﻿#include <iostream>
 
-void ShowConsoleCursor(bool showFlag)
+#include "Menu.hpp"
+
+std::tuple<int16_t, int16_t> configureConsole()
 {
 	HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
 
 	CONSOLE_CURSOR_INFO cursorInfo;
-
 	GetConsoleCursorInfo(out, &cursorInfo);
-	cursorInfo.bVisible = showFlag; // set the cursor visibility
+	cursorInfo.bVisible = false; // set the cursor visibility
 	SetConsoleCursorInfo(out, &cursorInfo);
+
+	CONSOLE_FONT_INFOEX cfi;
+	cfi.cbSize = sizeof(cfi);
+	cfi.nFont = 0;
+	cfi.dwFontSize.X = 0;                   // Width of each character in the font
+	cfi.dwFontSize.Y = 24;                  // Height
+	cfi.FontFamily = FF_DONTCARE;
+	cfi.FontWeight = FW_BOLD;
+	wcscpy_s(cfi.FaceName, L"Consolas"); // Choose your font
+	SetCurrentConsoleFontEx(out, FALSE, &cfi);
+
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+
+	return { csbi.srWindow.Right - csbi.srWindow.Left + 1, csbi.srWindow.Bottom - csbi.srWindow.Top + 1 };
 }
 
 int main()
 {
-	ShowConsoleCursor(false);
-	CONSOLE_SCREEN_BUFFER_INFO csbi;
-
-	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-	const int16_t columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
-	const int16_t rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+	const auto [columns, rows] = configureConsole();
 
 	try
 	{
